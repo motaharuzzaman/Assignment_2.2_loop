ff88b291f96d76d437dcd316b41d82593dc838ae
Md. Motaharuzzaman <motaharuzzaman71@gmail.com>
2023-10-04 19:21:51 +0600

    Question 1 & 2 completed

---
+++ assignment_2.2.py
@@ -0,0 +1,30 @@
+"""
+Problem 1: Print Even Numbers- Write a Python program that prints all even numbers
+from 1 to a given positive integer 'n'. Use a for loop to iterate through the numbers and
+print only the even ones.
+"""
+n = int(input('Enter any positive number:'))
+for i in range(1,n+1):
+    if i%2==0:
+        print(i)
+        i+=1
+
+"""
+Problem 2: Calculate Average- Write a Python program that calculates the average of
+a list of numbers. First, ask the user to input the number of values and then the values
+themselves. Use a for loop to calculate the average.
+"""
+n = int(input("Enter the number of values: "))
+print('Please input each value and press "Enter!"')
+sum=0
+for i in range(n):
+    value = float(input())
+    sum+=value
+    average = sum/n
+print('The average of the values is: ', average)
+
+"""
+Problem 3: Find Prime Numbers Write a Python program that finds and prints all
+prime numbers within a given range (from start to end). Use a for loop to check
+each number in the range for primality.
+"""
